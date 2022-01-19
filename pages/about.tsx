c7bc4bc966344f154e02e82b82f609e0cce55c49
Nahuel Castro <nahukas@gmail.com>
2022-01-19 15:37:51 +0100

    Added basic routing

---
+++ pages/about.tsx
@@ -0,0 +1,10 @@
+import Navbar from 'components/Navbar/Navbar';
+
+const About: React.FC = () => (
+  <div>
+    <Navbar />
+    <p>About Page!</p>
+  </div>
+);
+
+export default About;
---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Navbar from 'components/Navbar/Navbar';
+
+const Home: React.FC = () => (
+  <div>
+    <Navbar />
+    <h1>Hello World!</h1>
+  </div>
+);
+
+export default Home;
