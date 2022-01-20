2ba03d87ec50202809b070ada5a85a4d1df2c890
Nahuel Castro <nahukas@gmail.com>
2022-01-20 14:43:05 +0100

    Added app extension Created Layout

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,11 @@
+import Navbar from 'components/Navbar/Navbar';
+
+const Layout: React.FC = ({ children }) => (
+  <div>
+    <Navbar />
+    {children}
+    <footer>this is a footer</footer>
+  </div>
+);
+
+export default Layout;
